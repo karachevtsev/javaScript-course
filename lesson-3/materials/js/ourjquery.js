07832620724c5fddc97f4c60fe02e950e5a00dd2
Artem Karachevtsev <karachevtsev@yahoo.com>
2017-06-26 17:23:42 +0300

    Add materials for third lesson

--- lesson-3/materials/js/ourjquery.js
+++ lesson-3/materials/js/ourjquery.js
@@ -3,75 +3,61 @@
  * @param mixed some селектор || HTMLElement
  * @returns {OurJquery}
  */
-function $(some){
+
+function $(some) {
     var elements;
 
-    if(typeof some === "object") {
+    if (typeof some === 'object') {
         elements = [some];
-    }
-    else{
+    } else {
         elements = document.querySelectorAll(some);
     }
-    
-    return new OurJquery(elements);
-}
+
+     return new OurJquery(elements)
+ }
 
 function OurJquery(elements) {
+
     this.elements = elements;
 
     this.on = function (eventname, f) {
         for (var i = 0; i < this.elements.length; i++) {
             this.elements[i].addEventListener(eventname, f);
         }
-        
+
         return this;
-    }
-    
-    this.addClass = function(classname){
+    };
+
+    this.addClass= function (className) {
         for (var i = 0; i < this.elements.length; i++) {
-            this.elements[i].classList.add(classname);
+            this.elements[i].classList.add(className);
         }
-        
+
         return this;
-    }
-    
-    this.removeClass = function(classname){
+    };
+
+    this.removeClass= function (className) {
         for (var i = 0; i < this.elements.length; i++) {
-            this.elements[i].classList.remove(classname);
+            this.elements[i].classList.remove(className);
         }
-        
+
         return this;
-    }
-    
+    };
+
     /**
      * Получить или задать HTML-код
      * @param string html HTML код
      * @returns this || string, если html === undefined
      */
-    this.html = function(html){
-        if(typeof html === "undefined"){
+    this.html= function (html) {
+        if (typeof html === "undefined") {
             return this.elements[0].innerHTML;
         }
-        
         for (var i = 0; i < this.elements.length; i++) {
             this.elements[i].innerHTML = html;
         }
-        
+
         return this;
-    }
-    
-    this.fadeOut = function(time, callback){
-        /* 
-         * плавно уменьшает opacity с 1 до 0
-         * после делает display: none
-         * http://beta.dmitrylavrik.ru/blog/javascript/basics/prostaya-animaciya-na-javascript
-         * 
-         * 
-         * (*) callback вызывается после скрытия элемента :)
-         * (**) callback вызывается в контексте правильном :)
-         * 
-         * */
-        
-        
-    }
+    };
+
 }
--- lesson-3/materials/js/scripts.js
+++ lesson-3/materials/js/scripts.js
@@ -1,16 +1,20 @@
-$(window).on('load', function(e){
- 
-    $('.items .item').on('click', function(){
-        $(this).fadeOut(200);
-    }).addClass('shadow').html('123');
-    
-    console.log($('.items .item').html('123'));
-    console.log($('.items .item').html());
-    
-    $('.test .check').on('focus', function(){
+$(window).on('load', function (e) {
+    /*
+    функция $ возвразает объект $('.items .item')
+    у котрого есть метод on
+    в jQuery реализованы цепочки вида $(selector).addClass(className).on(eventname, f),
+    т.е. функции addClass, on - тоже вернули себя самих
+     */
+    $('.items .item').on('click', function () {
+        $(this).html('2');
+    });
+
+    $('.items .item').html('jQuery');
+
+    $('.items .item').addClass('shadow').html('magic');
+
+    $('.test .check').on('focus', function () {
         this.classList.add('focus');
-    }).on('blur', function(){
-        this.classList.remove('focus');
     });
 
 });
